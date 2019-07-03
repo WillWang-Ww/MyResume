666bee75df1f197bdfacd4838330a1db20b72a1f
WILLWANG <wangziweichn@outlook.com>
2019-07-03 21:47:17 +0800

    完成左边栏

--- js/main.js
+++ js/main.js
@@ -12,25 +12,24 @@ function writeCode(prefix,getCode,fn){
       window.clearInterval(timer)
       fn && fn.call()
     }
-  }, 0)
+  }, 10)
 }
 
-//add code to #content and style
-function writeContent(prefix,getCode,fn){
-  let domCode = document.querySelector('#content')
+//add code to content
+function writeContent(prefix,getCode,selector,fn){
+  let domCode = document.querySelector(selector)
   domCode.innerHTML = prefix || ''
   let n = 0
   let timer = setInterval(()=>{
     n += 1
-    domCode.innerHTML = Prism.highlight(prefix + getCode.substring(0,n), Prism.languages.css);
-    codeStyle.innerHTML = prefix + getCode.substring(0,n)
-    domCode.scrollTop = domCode.scrollHeight
+    domCode.innerHTML = prefix + getCode.substring(0,n)
     if (n >= getCode.length) {
       window.clearInterval(timer)
       fn && fn.call()
     }
-  }, 0)
+  }, 80)
 }
+
 
 var result=`/*
 *你好朋友，我是王子威
@@ -70,63 +69,100 @@ html{background: #f2f2f2;}
 `
 
 var result2=`
-#paper{
-  padding: 16px;
-  position: fixed;
-  width: 70%;
-  height: 100%;
-  right: 0;
-  display: flex;
-  align-items: flex-start;
-}
 #content{
   background-color: #e3edf7;
   width: 100%;
   height: 100%;
-  display:block;
+}
+/*
+*接下来向简历里面填充内容
+*左半栏是我的基本信息
+*/
+#personalInfo{
+  height: 100%;
+  width: 30%;
+  background-color: #6fa7da;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+}
+#portrait{
+  width: 180px;
+  height: 180px;
+  background-color:white;
+  border-radius: 50%;
+  margin:30px 15px;
 }
 `
+var result3 =`
+/*
+*null
+*/
+`
 
-
-writeCode('',result,()=>{
-  creatPaper(() => {
-    writeCode(result,result2,(()=>{
-      creatContent(()=>{
-        
-      })
+var text1 =`王子威`
+var text2 =`意向：前端工程师`
+var text3 = `
+出生日期：1996.09.02 <br>
+<br>
+邮箱：wangziweichn@outlook.com <br>
+<br>
+电话：16602060118 <br>
+<br>
+现居：深圳 <br>
+`
+var text4 = `教育经历`
+var text5 = `
+电子科技大学（985） 2014-2018
+智能电网信息工程
+`
+writeCode('',result,()=>{   //第一次写入，主要初始化写入框
+  creatPaper(() => {    //创建简历的DOM树
+    writeCode(result,result2,(()=>{   //第二次写入，主要完成简历左半栏样式
+        writeCode(result+result2,result3,()=>{
+          writeContent('',text1,'#name',()=>{
+            writeContent('',text2,'#job',()=>{
+              writeContent('',text3,'#contact')
+            })
+          })
+        })
     }))
   })
 })
 
 function creatPaper(fn){
+  //paper创建
   var paper = document.createElement('div')
   paper.id = 'paper'
   document.body.appendChild(paper)
   var content = document.createElement('div')
   content.id = 'content'
   paper.appendChild(content)
-  fn.call()
-}
 
-function creatContent(fn){
-  let content = document.querySelector('#content')
+  //content主要内容创建
   let personalInfo = document.createElement('div')
   let programInfo = document.createElement('div')
   personalInfo.id = 'personalInfo'
   programInfo.id = 'programInfo'
   content.appendChild(personalInfo)
   content.appendChild(programInfo)
+
+  //左半栏personalInfo的创建
   let portrait = document.createElement('div')
   portrait.id = 'portrait'
   personalInfo.appendChild(portrait)
   let name = document.createElement('div')
   name.id = 'name'
+  name.className = 'name'
   personalInfo.appendChild(name)
   let job = document.createElement('div')
   job.id = 'job'
+  job.className = 'job'
   personalInfo.appendChild(job)
   let contact = document.createElement('div')
   contact.id = 'contact'
+  contact.className = 'contact'
   personalInfo.appendChild(contact)
   let education = document.createElement('div')
   education.id = 'education'
@@ -143,6 +179,8 @@ function creatContent(fn){
   let major = document.createElement('div')
   major.id = 'major'
   personalInfo.appendChild(major)
+
+  //call下一个回调函数
   fn.call()
 }
 
